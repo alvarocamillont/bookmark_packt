c8ca86117b3103bc90e1bc43ebdbc0714f38f16d
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2017-09-29 14:28:52 -0300

    Desenvolvimento de login logout de padrão

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import url
-from . import views
+from django.contrib.auth.views import login, logout_then_login, logout
+from .views import dashboard
 
 
 urlpatterns = [
-    url(r'^login/$', views.user_login, name='login'),
+    # url(r'^login/$', views.user_login, name='login'),
+    url(r'^$', dashboard, name='dashboard'),
+
+    # login / logout urls
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, name='logout'),
+    url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
+
 ]
