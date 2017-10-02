3fddc2325b5436b9f0fe652348dcb6f29f179bbd
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2017-10-02 08:27:06 -0300

    Implementada troca de senha

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout_then_login, logout
+from django.contrib.auth import views
 from .views import dashboard
 
 
@@ -8,8 +8,11 @@ urlpatterns = [
     url(r'^$', dashboard, name='dashboard'),
 
     # login / logout urls
-    url(r'^login/$', login, name='login'),
-    url(r'^logout/$', logout, name='logout'),
-    url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^logout-then-login/$', views.logout_then_login, name='logout_then_login'),
 
+    # change password urls
+    url(r'^password-change/$', views.password_change, name='password_change'),
+    url(r'^password-change/done/$', views.password_change_done, name='password_change_done'),
 ]
