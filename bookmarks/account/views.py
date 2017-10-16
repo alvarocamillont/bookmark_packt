9ecec0c1fc911097e19f50a9e1652ed21091de01
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2017-10-16 09:22:58 -0200

    Criação da app action para rastrear a atividade do usuário

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -8,8 +8,8 @@ from django.views.decorators.http import require_POST
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile, Contact
 from common.decorators import ajax_required
-#from actions.models import Action
-#from actions.utils import create_action
+from actions.models import Action
+from actions.utils import create_action
 
 
 def user_login(request):
@@ -44,7 +44,7 @@ def register(request):
             new_user.save()
             # Create the user profile
             profile = Profile.objects.create(user=new_user)
-            #create_action(new_user, 'has created an account')
+            create_action(new_user, 'has created an account')
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -100,7 +100,7 @@ def user_follow(request):
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user,
                                               user_to=user)
-                #create_action(request.user, 'is following', user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user,
                                        user_to=user).delete()
